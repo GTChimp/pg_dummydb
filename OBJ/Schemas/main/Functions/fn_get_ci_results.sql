4db3f2778a2dad041938415e183c98c9c7af5dd6
YK <letscountheads@gmail.com>
2024-05-26 11:59:34 +0300

    func

---
+++ OBJ/Schemas/main/Functions/fn_get_ci_results.sql
@@ -0,0 +1,24 @@
+DROP FUNCTION IF EXISTS main.fn_get_ci_results;
+
+CREATE FUNCTION main.fn_get_ci_results(
+)
+    RETURNS TABLE
+    (
+        branch        main.log_ci_results.branch%TYPE,
+        deploy_mode   main.log_ci_results.deploy_mode%TYPE,
+        is_successful main.log_ci_results.is_successful%TYPE,
+        created       main.log_ci_results.created%TYPE
+    )
+AS
+$routine$
+BEGIN
+    RETURN QUERY SELECT
+                     lc.branch
+                     , lc.deploy_mode
+                     , lc.is_successful
+                     , lc.created
+                 FROM
+                     main.log_ci_results lc;
+END;
+$routine$
+    LANGUAGE plpgsql;
