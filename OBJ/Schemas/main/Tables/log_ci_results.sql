4a986e3b11fb3d8c08a486c759ebdcdd84b23d2b
YK <letscountheads@gmail.com>
2024-06-01 19:46:39 +0300

    +folder logging

--- OBJ/Schemas/main/Tables/log_ci_results.sql
+++ OBJ/Schemas/main/Tables/log_ci_results.sql
@@ -1,13 +1,19 @@
 CREATE TABLE IF NOT EXISTS main.log_ci_results (
-    branch        TEXT                             NOT NULL,
-    deploy_mode   TEXT                             NOT NULL,
-    is_successful BOOLEAN                          NOT NULL,
-    created       TIMESTAMP DEFAULT localtimestamp NOT NULL
+    branch          TEXT                             NOT NULL,
+    deploy_mode     TEXT                             NOT NULL,
+    deploy_type     TEXT                             NOT NULL,
+    requests_folder TEXT                             NOT NULL,
+    is_successful   BOOLEAN                          NOT NULL,
+    created         TIMESTAMP DEFAULT localtimestamp NOT NULL
 );
 
 COMMENT ON COLUMN main.log_ci_results.branch IS 'Branch/SHA-1';
 
 COMMENT ON COLUMN main.log_ci_results.deploy_mode IS 'Used deploy mode';
+
+COMMENT ON COLUMN main.log_ci_results.deploy_type IS 'Used deploy type';
+
+COMMENT ON COLUMN main.log_ci_results.requests_folder IS 'Release branch requests folder';
 
 COMMENT ON COLUMN main.log_ci_results.is_successful IS 'Success flag';
 
